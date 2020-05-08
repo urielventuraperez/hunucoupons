ebb3ccf53400bf3b73e83d90409376310e9c764b
uventura@simetrical.com.mx <uventura@simetrical.com.mx>
2020-05-08 12:57:42 -0500

    Bug Test helmet on facebook

--- src/components/seo/index.js
+++ src/components/seo/index.js
@@ -1,21 +1,21 @@
-import React from 'react';
+import React from "react";
 import { Helmet } from "react-helmet";
 import { APP_ROUTE } from "../../environments";
 
 const Seo = (props) => {
-
   const { title, discount, image, slug } = props;
 
-  return(
+  return (
     <Helmet>
       <title>{`Cuponesh - ${title}`}</title>
+      <meta name="description" content="Cuponesh" />
+      <meta property="og:type" content="article" />
       <meta property="og:title" content={title}></meta>
       <meta property="og:description" content={discount}></meta>
       <meta property="og:image" content={image}></meta>
       <meta property="og:url" content={`${APP_ROUTE}cupon/${slug}`}></meta>
     </Helmet>
-  )
-
-}
+  );
+};
 
 export default Seo;
